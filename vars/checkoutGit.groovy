921b2cc1c9e80a16f657f46327f61ca7897750e0
KeQingyuan <qingyuan.ke@foxmail.com>
2021-11-13 21:08:51 +0800

    修改checkoutGit增加子路径

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -5,6 +5,8 @@ import com.cloudogu.ces.cesbuildlib.Git
 def call(Map params = [:]){
     echo "从Git仓库检出源码"
     Git myGit = new Git(this,"")
-    myGit branch:"${params.branch}",url:"${params.url}",credentialsId:"${params.credentialsId}"
+    dir("${params.local}") {
+        myGit branch:"${params.branch}",url:"${params.url}",credentialsId:"${params.credentialsId}"
+    }
     echo "源码检出完成，${params.branch}"
 }
