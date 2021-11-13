f386bf2456e9050135ca220f2c680a2d22b88e69
KeQingyuan <qingyuan.ke@foxmail.com>
2021-11-13 12:44:48 +0800

    打印分支名称

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -6,5 +6,7 @@ def call(Map params = [:]){
     echo "从Git仓库检出源码"
     Git myGit = new Git(this,"")
     myGit branch:"${params.branch}",url:"${params.url}",credentialsId:"${params.credentialsId}"
-    echo "源码检出完成，${myGit.getBranchName()}"
+    // 获取检出的分支名称，以便打印
+    def branchName = myGit.getBranchName()
+    echo "源码检出完成，${branchName}"
 }
