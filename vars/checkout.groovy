809dc4fb143a71c05ca0458e12f5318bcab51152
KeQingyuan <qingyuan.ke@foxmail.com>
2021-11-13 10:44:42 +0800

    checkout.groovy 添加credentialsId

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -2,8 +2,8 @@ import com.cloudogu.ces.cesbuildlib.Git
 def call(Map config){
     node {
         echo 'Checkout...'
-        Git git = new Git(this, '${config.credentials}')
+        Git git = new Git(this)
 
-        git url: "${config.url}", branch: "${config.url}"
+        git url: "${config.url}", branch: "${config.url}", credentialsId:"${config.credentialsId}"
     }
 }
