98090b1c6e592e7d7698256533c98bfeda2f994c
KeQingyuan <qingyuan.ke@foxmail.com>
2021-11-13 11:01:52 +0800

    checkoutGit.groovy 单引号边双引号

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -3,7 +3,7 @@ def call(Map config){
     node {
         echo '从Git仓库检出源码'
         Git myGit = new Git(this)
-        myGit branch:'${config.branch}',url:'${config.url}',credentialsId:'${config.credentialsId}'
+        myGit branch:"${config.branch}",url:"${config.url}",credentialsId:"${config.credentialsId}"
         echo '源码检出完成，myGit.getBranchName()'
     }
 }
