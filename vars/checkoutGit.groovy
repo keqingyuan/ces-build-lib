59283e0538a8adc664f70de24458e5c95d0d944e
KeQingyuan <qingyuan.ke@foxmail.com>
2021-11-13 12:21:13 +0800

    去掉node包裹

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,9 +1,10 @@
+#!/usr/bin/env groovy
+
 import com.cloudogu.ces.cesbuildlib.Git
-def call(Map config){
-    node {
-        echo '从Git仓库检出源码'
-        Git myGit = new Git(this)
-        myGit branch:"${config.branch}",url:"${config.url}",credentialsId:"${config.credentialsId}"
-        echo '源码检出完成，myGit.getBranchName()'
-    }
+
+def call(Map params = [:]){
+    echo '从Git仓库检出源码'
+    Git myGit = new Git(this)
+    myGit branch:"${params.branch}",url:"${params.url}",credentialsId:"${params.credentialsId}"
+    echo '源码检出完成，myGit.getBranchName()'
 }
