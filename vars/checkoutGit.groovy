e670451c9e3e4269d0959a9bf2f36421d7beac84
KeQingyuan <qingyuan.ke@foxmail.com>
2021-11-13 12:30:20 +0800

    使用构造函数传递credentials

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -4,7 +4,7 @@ import com.cloudogu.ces.cesbuildlib.Git
 
 def call(Map params = [:]){
     echo '从Git仓库检出源码'
-    Git myGit = new Git(this)
-    myGit branch:"${params.branch}",url:"${params.url}",credentialsId:"${params.credentialsId}"
+    Git myGit = new Git(this,"${params.credentialsId}")
+    myGit branch:"${params.branch}",url:"${params.url}"
     echo '源码检出完成，myGit.getBranchName()'
 }
