7d69ca6d56ce4acf98df9349ef57b2f10baa9570
KeQingyuan <qingyuan.ke@foxmail.com>
2021-11-13 20:23:46 +0800

    打印从参数传递过来的参数

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -6,7 +6,5 @@ def call(Map params = [:]){
     echo "从Git仓库检出源码"
     Git myGit = new Git(this,"")
     myGit branch:"${params.branch}",url:"${params.url}",credentialsId:"${params.credentialsId}"
-    // 获取检出的分支名称，以便打印
-    def branchName = myGit.getBranchName()
-    echo "源码检出完成，${branchName}"
+    echo "源码检出完成，${params.branch}"
 }
